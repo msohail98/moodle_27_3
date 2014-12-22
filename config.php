32b31d64d88fc65dd8f221b35627349b3ccec424
Sohail Aslam <msohail98@yahoo.com>
2014-12-22 13:19:54 +1100

    Initial commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle_27_3_new';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = 'sohail786';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://localhost:8888/moodle_27_3_new';
+$CFG->dataroot  = '/Applications/MAMP/moodledata_27_3_new';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
